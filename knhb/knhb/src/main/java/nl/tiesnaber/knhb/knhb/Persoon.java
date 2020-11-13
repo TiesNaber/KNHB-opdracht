74c0f83229e97c2fa86b975d7b0bf33378e70933
TiesNaber <tiesnaber@live.nl>
2020-11-13 15:10:29 +0100

    Added Speler and SpelerRepo
    Implemented the speler class and its repsective repository. Made a small adjustment to the Persoon class --> Inheritance behaviour for database entities now get their own table

--- knhb/knhb/src/main/java/nl/tiesnaber/knhb/knhb/Persoon.java
+++ knhb/knhb/src/main/java/nl/tiesnaber/knhb/knhb/Persoon.java
@@ -1,19 +1,32 @@
 package nl.tiesnaber.knhb.knhb;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.text.DateFormat;
+import java.util.Date;
 
 @Entity // This tells Hibernate to make a table out of this class
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Persoon {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
     private String name;
-
+    private String geboorteDatum;
+    private String adres;
+    private String postcode;
+    private String plaats;
     private String email;
+
+
+    public Persoon(String name, String geboorteDatum, String adres, String postcode, String plaats, String email) {
+        this.name = name;
+        this.geboorteDatum = geboorteDatum;
+        this.adres = adres;
+        this.postcode = postcode;
+        this.plaats = plaats;
+        this.email = email;
+    }
 
     public Integer getId() {
         return id;
---
+++ knhb/knhb/src/main/java/nl/tiesnaber/knhb/knhb/Speler.java
@@ -0,0 +1,43 @@
+package nl.tiesnaber.knhb.knhb;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Speler extends Persoon{
+
+    private int Club_ID;
+    private int Team_ID;
+
+    private boolean speelgerechtigd;
+
+    public Speler(String name, String geboorteDatum, String adres, String postcode, String plaats, String email, boolean speelgerechtigd) {
+        super(name, geboorteDatum, adres, postcode, plaats, email);
+        this.speelgerechtigd = speelgerechtigd;
+    }
+
+    public int getClub_ID() {
+        return Club_ID;
+    }
+
+    public void setClub_ID(int club_ID) {
+        Club_ID = club_ID;
+    }
+
+    public int getTeam_ID() {
+        return Team_ID;
+    }
+
+    public void setTeam_ID(int team_ID) {
+        Team_ID = team_ID;
+    }
+
+    public boolean isSpeelgerechtigd() {
+        return speelgerechtigd;
+    }
+
+    public void setSpeelgerechtigd(boolean speelgerechtigd) {
+        this.speelgerechtigd = speelgerechtigd;
+    }
+}
