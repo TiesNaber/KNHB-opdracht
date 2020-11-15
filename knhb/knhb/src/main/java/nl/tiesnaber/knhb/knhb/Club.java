e542398b39ba49cbe4f98e05a47cbd66d8e22def
TiesNaber <tiesnaber@live.nl>
2020-11-15 16:12:04 +0100

    Added club entity

---
+++ knhb/knhb/src/main/java/nl/tiesnaber/knhb/knhb/Club.java
@@ -0,0 +1,80 @@
+package nl.tiesnaber.knhb.knhb;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Club {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Integer club_ID;
+
+    private String naam;
+    private String adres;
+    private String plaats;
+    private String postcode;
+    private String telefoonNummer;
+    private String email;
+
+    public Club(String naam, String adres, String plaats, String postcode, String telefoonNummer, String email) {
+        this.naam = naam;
+        this.adres = adres;
+        this.plaats = plaats;
+        this.postcode = postcode;
+        this.telefoonNummer = telefoonNummer;
+        this.email = email;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public String getPlaats() {
+        return plaats;
+    }
+
+    public void setPlaats(String plaats) {
+        this.plaats = plaats;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getTelefoonNummer() {
+        return telefoonNummer;
+    }
+
+    public void setTelefoonNummer(String telefoonNummer) {
+        this.telefoonNummer = telefoonNummer;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ knhb/knhb/src/main/java/nl/tiesnaber/knhb/knhb/ClubRepo.java
@@ -0,0 +1,6 @@
+package nl.tiesnaber.knhb.knhb;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClubRepo extends CrudRepository<Club,Integer> {
+}
