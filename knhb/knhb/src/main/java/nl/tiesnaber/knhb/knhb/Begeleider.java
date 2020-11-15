49d7db516661a9a861e35fc3d9835dc1bbbb5d25
TiesNaber <tiesnaber@live.nl>
2020-11-15 15:53:02 +0100

    Added Begeleider
    Created the Begeleider class and its corresponding Repo interface.

---
+++ knhb/knhb/src/main/java/nl/tiesnaber/knhb/knhb/Begeleider.java
@@ -0,0 +1,22 @@
+package nl.tiesnaber.knhb.knhb;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Begeleider extends Persoon {
+
+    private int rol_ID;
+
+    public Begeleider(String name, String geboorteDatum, String adres, String postcode, String plaats, String email, int rol_ID) {
+        super(name, geboorteDatum, adres, postcode, plaats, email);
+        this.rol_ID = rol_ID;
+    }
+
+    public int getRol_ID() {
+        return rol_ID;
+    }
+
+    public void setRol_ID(int rol_ID) {
+        this.rol_ID = rol_ID;
+    }
+}
